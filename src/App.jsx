c623eaae172fc30a442c6f7774fd61e817f20305
mozta <perezaguirrerafael@gmail.com>
2024-11-04 17:58:09 -0600

    firebase services

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import { Menu } from "./components/Menu";
+import { Orders } from "./components/Orders";
+
 
 function App() {
   return (
     <>
       <Menu />
+      <Orders />
     </>
   );
 }
